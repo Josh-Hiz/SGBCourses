d14b2bdb5ed37db083cafb4f4b7719aa96f76590
Josh H <jhizgiaev2704@gmail.com>
2023-07-06 21:46:22 -0400

    Overrite the default css for code blocks

--- conf.py
+++ conf.py
@@ -24,6 +24,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
 
+html_css_files = [
+    'css/code_block.css',
+]
+
 html_theme_options = {
     "home_page_in_toc": True,
 }
